9b62e07df39177b5f5019e5121fbb0b7298ae2e3
y_d_k@live.de <kutz@ki.informatik.uni-frankfurt.de>
2018-02-02 18:40:17 +0100

    Rename main to Main

--- main/Permutation.hs
+++
@@ -1,7 +0,0 @@
-module Permutation where
-class Eq e => PrePermutation e where
-  identity :: e
-
-class PrePermutation e => Permutation e where
-  inverse :: e -> e
-  add :: e -> e -> e         -- add pi1 pi2 == pi1 comp pi2
